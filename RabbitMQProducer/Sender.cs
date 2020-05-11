ccb69f3ae066488e7fb90c250a09c94d70385e2f
Adekunle Alugbin <alugbin96@gmail.com>
2020-05-11 18:41:16 +0100

    Add project files.

---
+++ RabbitMQProducer/Sender.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQProducer
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" }; //where the RabbtiMqService is running
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "Test"); //Creates a queue
+
+
+            var message = "I am testing RabbitMQ";
+            var body = Encoding.UTF8.GetBytes(message);
+
+
+            Console.WriteLine("Sending message.....................");
+            channel.BasicPublish("", "Test", null, body);
+            Console.WriteLine("Message Sent!");
+            Console.ReadLine();
+
+        }
+    }
+}
